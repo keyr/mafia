6524ed529d77bbcc2cf1f72ef265b6d89a000c0b
keyr <54866965+keyr@users.noreply.github.com>
2020-08-30 22:27:30 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 import Firebase from 'firebase';
 let config = {
-    apiKey: "AIzaSyAZFDlrfAAQGv9TTiIY_E6WdBXxccwjhh0",
-    authDomain: "mafia-37f83.firebaseapp.com",
-    databaseURL: "https://mafia-37f83.firebaseio.com",
-    projectId: "mafia-37f83",
-    storageBucket: "mafia-37f83.appspot.com",
-    messagingSenderId: "34413760531",
+    apiKey: "API-KEY",
+    authDomain: "AUTHENTICATION-DOMAIN",
+    databaseURL: "DATABASE-URL",
+    projectId: "PROJECT-ID",
+    storageBucket: "STORAGE-BUCKET",
+    messagingSenderId: "MESSAGE-SENDER-ID",
 };
 let app = Firebase.initializeApp(config);
 export const db = app.database();
